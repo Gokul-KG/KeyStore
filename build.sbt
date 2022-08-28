0eb664f5b5ba53d20c2fa12c0847b4dccc985f57
Gokul <gokul@Gokuls-MacBook-Pro.local>
2022-08-29 02:18:04 +0530

    added basic test cases

--- build.sbt
+++ build.sbt
@@ -8,5 +8,5 @@ lazy val root = (project in file("."))
     libraryDependencies ++= Seq(
 	"org.scalatest" %% "scalatest" % "3.0.0" % "test",
 	"org.scalacheck" %% "scalacheck" % "1.13.4" % "test"
-)
+    )
   )
---
+++ src/test/scala/btree/BasicTest.scala
@@ -0,0 +1,43 @@
+package btree
+
+import java.io.{File, RandomAccessFile, ByteArrayInputStream, ByteArrayOutputStream, DataInputStream, DataInput, DataOutputStream, DataOutput}
+
+import org.scalatest.FunSuite
+
+class BasicSuite extends FunSuite {
+  
+
+   test("testing persistance with Integer key") {
+    
+    val file = "file"
+    val t = new FileBPlusTree[Int, String]( file, 3)
+    t.insert(1, Seq(3, 4, 5) )
+	t.close
+		
+	val t1 = FileBPlusTree[Int, Any]( file )	
+	
+
+    assert(t1.search(1).get == Seq(3, 4, 5))
+    new File(file).delete()
+  }
+
+  test("testing persistance with String key") {
+    
+    val file = "file"
+    val key = "test"
+    val t = new FileBPlusTree[String, Any]( file, 3)
+    t.insert(key, Seq(3, 4, 5) )
+	t.close
+		
+	val t1 = FileBPlusTree[String, Any]( file )	
+	
+
+    assert(t1.search(key).get == Seq(3, 4, 5))
+    new File(file).delete()
+  }
+
+}
+
+
+
+
